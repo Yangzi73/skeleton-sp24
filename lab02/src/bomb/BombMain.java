8aa30e8fc920915ea85585622d0a53399b5d6c67
Yang <1142190666@qq.com>
2025-05-05 13:03:08 +0800

    complete the Lab02(Debugging)

--- lab02/src/bomb/BombMain.java
+++ lab02/src/bomb/BombMain.java
@@ -4,6 +4,7 @@ import common.IntList;
 
 public class BombMain {
     public static void answers(String[] args) {
+        //IntList passwordPhase1 = new IntList(0,new IntList(9,new IntList(3,new IntList(0,new IntList(8,null)))));
         int phase = 2;
         if (args.length > 0) {
             phase = Integer.parseInt(args[0]);
@@ -11,13 +12,13 @@ public class BombMain {
         // TODO: Find the correct inputs (passwords) to each phase using debugging techniques
         Bomb b = new Bomb();
         if (phase >= 0) {
-            b.phase0("Figure this out. I wonder where the phases are defined...");
+            b.phase0("39291226");
         }
         if (phase >= 1) {
-            b.phase1(null); // Figure this out too
+            b.phase1(IntList.of(0,9,3,0,8)); // Figure this out too
         }
         if (phase >= 2) {
-            b.phase2("Figure this out. I wonder where the phases are defined...");
+            b.phase2("-81201430");
         }
     }
 }
